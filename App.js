8adc39963acb9712ea538686a0041f0a7ac53e16
Ashish Subedi <ashishsubedi10@outlook.com>
2019-07-05 09:32:02 +0545

    Added Navigators

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { Home, Stats, Profile, Budget, Dashboard } from './src/components';
-import { Income, Expense } from './src/components/screens';
-import { MonthlyIncomeChart, MonthlySpentChart } from './src/components/Charts'
+// import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { Home, Stats, Profile, Budget } from './src/components';
+import { Income, Expense, AddTransactions } from './src/components/screens';
+// import { MonthlyIncomeChart, MonthlySpentChart } from './src/components/Charts'
+
+
+//React navigation
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+
 
 
 
@@ -36,63 +41,63 @@ export default class App extends Component {
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 3,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 4,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 5,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 6,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 7,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 8,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 9,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 10,
           category: 'Food And Drinks',
           title: 'Khaja',
           amount: 60,
           currency: 'Rs',
           date: Date.now()
-        },{
+        }, {
           id: 11,
           category: 'Food And Drinks',
           title: 'Khaja',
@@ -102,11 +107,40 @@ export default class App extends Component {
         },
       ]
     }
+
+
   }
   render() {
-    
+
     return (
-      <Home />
+      <AppContainer />
     )
   }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeStackNavigator,
+  Stats: StatsStackNavigator,
+  Budget: BudgetStackNavigator,
+  Profile: ProfileStackNavigator,
+});
+
+const HomeStackNavigator = createStackNavigator({
+  Home,
+  Income,
+  Expense,
+  AddTransactions
+});
+
+const StatsStackNavigator = createStackNavigator({
+  Stats,
+});
+
+const BudgetStackNavigator = createStackNavigator({
+  Budget,
+});
+const ProfileStackNavigator = createStackNavigator({
+  Profile,
+});
+
+const AppContainer = createAppContainer(TabNavigator);
